0464b01c5d7358e0edf4ede4a09bf957108edb9f
koki-higami <koki.higami@paypay-corp.co.jp>
2023-06-23 10:56:29 +0900

    fix : 画面表示項目修正

--- src/main/java/com/example/sampledemo/controller/dto/response/UserPurchaseHistoriesResponse.java
+++ src/main/java/com/example/sampledemo/controller/dto/response/UserPurchaseHistoriesResponse.java
@@ -1,20 +1,34 @@
 package com.example.sampledemo.controller.dto.response;
 
 import com.example.sampledemo.service.dto.response.PurchaseHistoriesFindResponse;
+import com.example.sampledemo.service.dto.response.UserFindResponse;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class UserPurchaseHistoriesResponse {
 
-  UserInfoResponse userInfo;
+  PurchaseUser userInfo;
   List<UserPurchaseHistoryResponse> histories;
 
+  @Getter
+  static class PurchaseUser {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+
+    public PurchaseUser(UserFindResponse data) {
+      this.userId = data.getUserId();
+      this.firstName = data.getFirstName();
+      this.lastName = data.getLastName();
+    }
+  }
   public UserPurchaseHistoriesResponse(PurchaseHistoriesFindResponse data) {
 
     this.histories =
-        data.getHistories().stream().map(responseHistory -> new UserPurchaseHistoryResponse(responseHistory)).toList();
+        data.getHistories().stream().map(UserPurchaseHistoryResponse::new).toList();
 
-    this.userInfo = new UserInfoResponse(data.getUserInfo());
+    UserFindResponse userData = data.getUserInfo();
+    this.userInfo = new PurchaseUser(userData);
   }
 }
--- src/main/java/com/example/sampledemo/controller/dto/response/UserPurchaseHistoryResponse.java
+++ src/main/java/com/example/sampledemo/controller/dto/response/UserPurchaseHistoryResponse.java
@@ -6,14 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class UserPurchaseHistoryResponse {
-  private Integer userId;
   private LocalDate purchaseDate;
   private String itemName;
   private Integer itemAmount;
 
 
   public UserPurchaseHistoryResponse(PurchaseHistoryFindResponse data) {
-    this.userId = data.getUserId();
     this.purchaseDate = data.getPurchaseDate();
     this.itemName = data.getItemName();
     this.itemAmount = data.getItemAmount();
